49904149f017d38e07155d511707649ea7fa8e3a
Zhiyuan_Ma <zm2354@columbia.edu>
2025-03-02 16:09:37 +0800

    update

--- example.py
+++ example.py
@@ -17,6 +17,17 @@ num_results_per_prompt = 1
 seed = 42
 device = "cuda"
 
+# download pretrained models if not exist
+if not os.path.exists(adapter_name_or_path):
+    print(f"Downloading pretrained models from huggingface")
+    os.system(
+        f"huggingface-cli download --resume-download ZhiyuanthePony/TriplaneTurbo \
+        --include \"triplane_turbo_sd_v1.pth\" \
+        --local-dir ./pretrained \
+        --local-dir-use-symlinks False"
+        )
+
+
 # Initialize the TriplaneTurbo pipeline
 triplane_turbo_pipeline = TriplaneTurboTextTo3DPipeline.from_pretrained(adapter_name_or_path)
 triplane_turbo_pipeline.to(device)
@@ -35,4 +46,4 @@ for i, mesh in enumerate(output["mesh"]):
     name = f"{prompt.replace(' ', '_')}_{i}"
     save_paths = export_obj(mesh, f"{output_dir}/{name}.obj")
     print(f"Saved mesh to: {save_paths}")
-    
+
