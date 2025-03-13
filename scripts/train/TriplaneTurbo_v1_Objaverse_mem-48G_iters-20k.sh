a53563315ba46ae27294614e32d4d96dac5342ef
Zhiyuan_Ma <zm2354@columbia.edu>
2025-03-13 15:01:19 +0800

    update

--- scripts/train/TriplaneTurbo_v1_Objaverse_mem-48G_iters-20k.sh
+++ scripts/train/TriplaneTurbo_v1_Objaverse_mem-48G_iters-20k.sh
@@ -1,5 +1,5 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python launch.py \
-    --config configs/v1.yaml  \
+CUDA_VISIBLE_DEVICES=2 python launch.py \
+    --config configs/v1_mem-48G_iters-20k.yaml  \
     --train \
     data.prompt_library="3DTopia_361k_prompt_library" \
     data.condition_processor.cache_dir=".threestudio_cache/text_embeddings_3DTopia_361k" \
